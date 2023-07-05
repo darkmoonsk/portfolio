62f7c9e7ed6dcebd3f67727ac2c54672746fe8ff
Bruno Souza <brunoezully@gmail.com>
2023-07-04 21:33:31 -0300

    Feat: Add seção sobre mim

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,28 @@
+import Projects from "../../components/templates/Projects";
 import ProjectCard from "../../components/projectCard/ProjectCard";
-import ProjectCardInverted from "../../components/projectCard/ProjectCardInverted";
 import styles from "./Home.module.css";
+import About from "../../components/about/About";
 
 function Home() {
     return (
         <div className={styles.home}>
-            <ProjectCard 
-                imgUrl="./images/projects/disneyplus-3-devices-black.png" 
-                url="http://disneyplus.brunosouzadev.com/"
-                name="Disney+"
-                description="Recriação do front-end do Disney+"
-            />
-            <ProjectCardInverted
-                imgUrl="./images/projects/worldcup-3-devices-black.png" 
-                url="http://worldcup22.brunosouzadev.com/"
-                name="World Cup"
-                description="Simulador da copa do mundo 2022"
-            />
-        </div>
+            <Projects>
+                <ProjectCard 
+                    imgUrl="./images/projects/disneyplus-3-devices-black.png" 
+                    url="http://disneyplus.brunosouzadev.com/"
+                    name="Disney+"
+                    description="Recriação do front-end do Disney+"
+                />
+                <ProjectCard
+                    inverted
+                    imgUrl="./images/projects/worldcup-3-devices-black.png" 
+                    url="http://worldcup2022.brunosouzadev.com/"
+                    name="World Cup"
+                    description="Simulador da copa do mundo 2022"
+                />
+            </Projects>
+            <About />
+        </div>     
     );
 }
 

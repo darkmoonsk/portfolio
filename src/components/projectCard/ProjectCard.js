4b7ce7f8ed04c01f1579114f2c6f4b595e2db3fc
Bruno Souza <brunoezully@gmail.com>
2023-07-04 21:31:57 -0300

    Refactor: Ajustes no components de card, +modular

--- src/components/projectCard/ProjectCard.js
+++ src/components/projectCard/ProjectCard.js
@@ -3,11 +3,19 @@ import styles from "./ProjectCard.module.css";
 function ProjectCard(props) {
   return (
     <div className={styles.container}>
-        <img alt="Imagem do projeto" src={props.imgUrl} />
+        { !props.inverted ? 
+          <img alt="Imagem do projeto" src={props.imgUrl} />
+        : <></>}
         <div className={styles.info}>
             <h2>{props.name}</h2>
             <p>{props.description}</p>
+            <a href={props.url} target="_blanc">
+                  <button>Ver projeto</button>
+            </a>
         </div>
+        { props.inverted ? 
+          <img alt="Imagem do projeto" src={props.imgUrl} />
+        : <></>}
     </div>
   )
 }
